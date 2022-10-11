0f7c41eb89132146e73b13cf6f21582463e81fea
Aevin Jais <aevin.jais@kcl.ac.uk>
2022-10-12 00:19:53 +0100

    Add functionality for prime numbers searching

--- primes.py
+++ primes.py
@@ -1,6 +1,20 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+import math
+
 def primes(number_of_primes):
     list = []
+    count = 2
+    while len(list)<number_of_primes:
+        if isPrime(count):
+            list.append(count)
+        count+=1
     return list
+
+
+def isPrime(n):
+    for i in range(2,int(math.sqrt(n))+1):
+        if (n%i) == 0:
+            return False
+    return True
